65709220eba98582c18f1d8f6edaa0dc38f4f61c
Brian <bbattell@gmail.com>
2018-12-18 22:33:03 -0500

    added reservations form

--- app/reservation/reservation.component.ts
+++ app/reservation/reservation.component.ts
@@ -6,6 +6,11 @@ import { ModalDialogService, ModalDialogOptions } from "nativescript-angular/mod
 import { ReservationModalComponent } from "../reservationmodal/reservationmodal.component";
 import * as app from "application";
 import { RadSideDrawer } from "nativescript-ui-sidedrawer";
+import { Page } from "ui/page";
+import { View } from "ui/core/view";
+import { Animation, AnimationDefinition } from "ui/animation";
+import { CouchbaseService } from '../services/couchbase.service';
+import * as enums from "ui/enums";
 
 @Component({
     selector: 'app-reservation',
@@ -16,9 +21,14 @@ import { RadSideDrawer } from "nativescript-ui-sidedrawer";
 export class ReservationComponent implements OnInit {
 
     reservation: FormGroup;
+    showFormCard: boolean = true;
+    showResultCard: boolean = false;
+    formCard: View;
+    resultsCard: View;
+    docId: string = "reservations";
 
     constructor(private formBuilder: FormBuilder, private modalService: ModalDialogService,
-    private vcRef: ViewContainerRef) {
+    private vcRef: ViewContainerRef, private couchbaseService: CouchbaseService) {
 
             this.reservation = this.formBuilder.group({
                 guests: 3,
@@ -79,5 +89,49 @@ export class ReservationComponent implements OnInit {
 
     onSubmit() {
         console.log(JSON.stringify(this.reservation.value));
+        //Store form data here.
+        let doc = this.couchbaseService.getDocument(this.docId);
+        if( doc == null) {
+          this.couchbaseService.createDocument({"reservations": [{
+            "guests": string,
+            "smoking": boolean,
+            "dateTime": string
+          }]}, this.docId);
+        }
+        else {
+          this.reservation = doc.reservation;
+        }
+
+        let req = this.reservation.value;
+
+        this.couchbaseService.updateDocument(this.docId, {
+          "guests": this.req.reservations[0],
+          "smoking": this.req.reservations[1],
+          "dateTime": this.req.reservations[2]});
+
+
+
+        this.formCard = this.page.getViewById<View>('formCard');
+        this.resultsCard = this.page.getViewById<View>('resultCard');
+
+        this.formCard.animate({
+          scale: { x: 0.2, y: 0.2},
+          opacity: 0.1,
+          duration: 500
+        }).then(() => {
+          this.resultsCard.animate({
+            scale: { x: 0.2, y: 0.2},
+            opacity: 0.1,
+            duration: 500
+          }).then(() => {
+            this.showFormCard = false;
+            this.showResultCard = true;
+            this.resultsCard.animate({
+              scale: { x: 1, y: 1},
+              opacity: 1,
+              duration: 500
+            });
+          });
+        });
     }
 }
